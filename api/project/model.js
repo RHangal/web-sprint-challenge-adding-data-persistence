7db62f6947e563fbb6b71a40a0a51f8ef50fa64e
Rohan Hangal <rohanhangal@gmail.com>
2023-06-27 21:38:10 -0700

    projects router done

--- api/project/model.js
+++ api/project/model.js
@@ -1,1 +1,10 @@
 // build your `Project` model here
+const db = require("../../data/dbConfig");
+
+function find() {
+  return db("projects");
+}
+
+module.exports = {
+  find,
+};
--- api/project/router.js
+++ api/project/router.js
@@ -1,6 +1,20 @@
 // build your `/api/projects` router here
 const express = require("express");
 
+const Projects = require("./model");
+
 const router = express.Router();
+
+router.get("/projects", (req, res, next) => {
+  Projects.find()
+    .then((projects) => {
+      const something = projects.map((proj) => ({
+        ...proj,
+        project_completed: Boolean(proj.project_completed),
+      }));
+      res.json(something);
+    })
+    .catch(next);
+});
 
 module.exports = router;
